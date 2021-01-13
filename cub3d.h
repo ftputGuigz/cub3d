89792281135520f45e667316e6cfced5d521d5a7
Guillaume PETIT <gpetit@e1r2p22.42.fr>
2021-01-13 17:47:23 +0100

    debut de parseur de map

---
+++ cub3d.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpetit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 14:43:05 by gpetit            #+#    #+#             */
+/*   Updated: 2021/01/13 16:27:28 by gpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <string.h>
+# include <fcntl.h>
+
+# include "get_next_line.h"
+
+typedef struct	s_struct
+{
+	int res_x;
+	int res_y;
+	char *NO_path;
+	char *SO_path; 
+	char *EA_path;
+	char *WE_path; 
+	char *sprite_path;
+	char *floor_path;
+	char *ceiling_path;
+}				t_map;
+
+#endif
--- parsor.c
+++ parsor.c
@@ -1,13 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsor.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpetit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/13 14:41:21 by gpetit            #+#    #+#             */
-/*   Updated: 2021/01/13 14:41:24 by gpetit           ###   ########.fr       */
+/*   Created: 2021/01/13 14:35:23 by gpetit            #+#    #+#             */
+/*   Updated: 2021/01/13 16:28:13 by gpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d.h"
 
+int ft_fillstruct(char *line)
+{
+	t_map map_datas;
+
+	if ()
+}
+
+int	main(void)
+{
+	int fd;
+	int a;
+	char *line;
+
+	fd = open("map.cub", O_RDONLY);
+	a = get_next_line(fd, &line);
+	while (a)
+	{
+		printf("line = %s\n", line);
+		
+		a = get_next_line(fd, &line);
+	}
+	close(fd);
+	return(0);
+}
