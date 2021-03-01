d0821b3e9e2ff955cb397054b577e536661b57f2
Guillaume PETIT <gpetit@e2r5p18.42.fr>
2021-03-01 16:43:38 +0100

    map_correction added, testing it on VM

--- srcs/map_parsor.c
+++ srcs/map_parsor.c
@@ -81,6 +81,31 @@ static int	register_sprites(t_malloc *m, t_datas *map)
 	return (0);
 }
 
+static void	map_correction(t_datas *map)
+{
+	int k;
+	int i;
+	char *tmp;
+	char *tmp2;
+
+	k = 0;
+	while (map->map[k])
+	{
+		if ((int)(ft_strlen(map->map[k])) < map->columns)
+		{
+			tmp = malloc(sizeof(char) * (map->columns - (int)(ft_strlen(map->map[k]))));
+			i = 0;
+			while (i < (map->columns - (int)(ft_strlen(map->map[k]))))
+				tmp[i++] = ' ';
+			tmp[i] = '\0';
+			tmp2 = map->map[k];
+			map->map[k] = ft_strjoin(tmp2, tmp);
+			free(tmp2);
+		}
+		k++;
+	}
+}
+
 int			map_parsor(t_malloc *m, t_datas *map)
 {
 	static int	k = 0;
@@ -97,6 +122,7 @@ int			map_parsor(t_malloc *m, t_datas *map)
 	{
 		register_map_datas(map);
 		ret = register_sprites(m, map);
+		map_correction(map);
 		if (ret)
 			return (-1);
 	}
